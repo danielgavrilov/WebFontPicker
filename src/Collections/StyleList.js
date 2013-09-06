a3ea2cb19a84cb34c33389a69de60ea4be377b8c
Daniel Gavrilov <drifter4e@gmail.com>
2013-09-06 01:06:06 +0100

    Converted indentation to spaces

--- src/Collections/StyleList.js
+++ src/Collections/StyleList.js
@@ -1,37 +1,37 @@
 var StyleList = Backbone.Collection.extend({
-	
-	model: Style,
+    
+    model: Style,
 
-	initialize: function() {
-		_.bind(this, 'addNew');
-		this.on('remove', this.addIfEmpty);
-		this.addIfEmpty();
-	},
+    initialize: function() {
+        _.bind(this, 'addNew');
+        this.on('remove', this.addIfEmpty);
+        this.addIfEmpty();
+    },
 
-	// Adds a new blank model to the collection (and selects it).
-	addNew: function() {
-		var model = new Style;
+    // Adds a new blank model to the collection (and selects it).
+    addNew: function() {
+        var model = new Style;
         this.add(model).select(model);
-	},
+    },
 
-	// Adds a new model if the collection is empty.
-	addIfEmpty: function() {
-		if (this.length === 0) {
-			this.addNew();
-		}
-	},
+    // Adds a new model if the collection is empty.
+    addIfEmpty: function() {
+        if (this.length === 0) {
+            this.addNew();
+        }
+    },
 
-	// Returns the model that is selected.
-	selected: function() {
-		return this.findWhere({ selected: true });
-	},
+    // Returns the model that is selected.
+    selected: function() {
+        return this.findWhere({ selected: true });
+    },
 
-	// Selects a model.
-	select: function(model) {
-		if (model.get('selected')) return;
-		var selected = this.selected();
-		if (selected) selected.set('selected', false);
-		model.set('selected', true);
-	}
+    // Selects a model.
+    select: function(model) {
+        if (model.get('selected')) return;
+        var selected = this.selected();
+        if (selected) selected.set('selected', false);
+        model.set('selected', true);
+    }
 
 });
